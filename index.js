63e6d288f23d5ba6d7d82b8406e421bad60dea6d
Andrew <aforest41@yahoo.com>
2022-11-25 09:14:30 -0800

    commit tree c56e7925d7b51d32b74073666c690cdd614f5f6c

---
+++ index.js
@@ -0,0 +1,8 @@
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2,3); //=> 5
+
+const divide = (num1, num2) => num1 / num2;
+divide(2000,100);
+
+const square = num => num * num;
+
